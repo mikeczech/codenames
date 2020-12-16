53ce44e43e40bf1782513d7a3ea3cb0c6c3aec9a
Mike Czech <mail@mczech.de>
2020-12-16 21:06:20 +0100

    First version

--- notebooks/word2vec.py
+++ notebooks/word2vec.py
@@ -1,7 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,md,py:percent
+#     formats: ipynb,py:percent
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -22,67 +22,122 @@
 # %%
 import spacy
 import numpy as np
-import umap
-import altair as alt
-import pandas as pd
+from tqdm.notebook import tqdm
+
+from itertools import combinations
+
+# %%
+with open("../data/words_alpha.txt") as f:
+    words = f.readlines()
+words = [w.strip() for w in words]
+
+# %%
+len(words)
 
 # %%
 nlp = spacy.load("en_core_web_lg")
 
 # %%
-len(list(nlp.vocab.strings))
+nlp.meta["vectors"]
+
+# %%
+india = nlp.vocab.strings["india"]
+germany = nlp.vocab.strings["germany"]
+apple = nlp.vocab.strings["apple"]
+
+# %%
+nlp.vocab[india].similarity(nlp.vocab[germany])
+
+# %%
+nlp.vocab[india].similarity(nlp.vocab[apple])
+
+# %%
+list(combinations(red_words, 2))
 
 
 # %%
-def get_vector(word):
-    return np.asarray([nlp.vocab.vectors[nlp.vocab.strings[word]]])
-
-
-def most_similar(words, n=400):
-    words = [w.lower() for w in words]
-    centroid = np.mean([get_vector(w) for w in words], axis=0)
-    most_sim_indices = nlp.vocab.vectors.most_similar(centroid, n=n)
-    suggestions = [nlp.vocab.strings[w].lower() for w in most_sim_indices[0][0]]
-
-    res = dict()
-    for s in suggestions:
-        if not any([w in s for w in words]):
-            res[s] = get_vector(s)
-
-    for w in words:
-        res[w] = get_vector(w)
-
-    return res
+def similarity(word_a, word_b):
+    word_a = nlp.vocab.strings[word_a]
+    word_ab = nlp.vocab.strings[word_b]
+    return nlp.vocab[word_a].similarity(nlp.vocab[word_b])
 
 
 # %%
-words = ["india", "germany", "usa", "spain"]
+red_words = [
+    "arm",
+    "stick",
+    "organ",
+    "field",
+    "piano",
+    "agent",
+    "leprechaun",
+    "swing",
+    "india",
+]
+
+blue_words = [
+    "roulette",
+    "stock",
+    "dinosaur",
+    "embassy",
+    "hole",
+    "turkey",
+    "fire",
+    "shakespeare",
+]
+
+neutral_words = ["jack", "point", "laser", "lemon", "unicorn", "pyramid", "figure"]
+assassin_words = ["hollywood"]
+
+all_words = red_words + blue_words + neutral_words + assassin_words
 
 # %%
-ms = most_similar(words)
+X = np.array([[similarity(w_a, w) for w in words] for w_a in all_words])
 
 # %%
-X = np.asarray(list(ms.values())).squeeze()
 X.shape
 
-# %%
-reducer = umap.UMAP()
-embedding = reducer.fit_transform(X)
 
 # %%
-df_emb = (
-    pd.DataFrame(embedding)
-    .rename({0: "x", 1: "y"}, axis=1)
-    .assign(
-        label=list(ms.keys()),
-        color=[0] * (len(embedding) - len(words)) + [1] * len(words),
-    )
-)
-df_emb
+def is_valid(suggested_word):
+    for w in all_words:
+        if (suggested_word in w) or (w in suggested_word):
+            return False
+    return True
+
+
+def produce_clue(red_words, blue_words, neutral_words, assassin_words):
+    N = len(red_words)
+    word = None
+    best_c = None
+    max_avg_sim = -np.inf
+
+    for l in range(2, 3):
+        for c in combinations(range(N), l):
+            inds = X[np.array(c)].mean(axis=0).argsort()[::-1][:15]
+            avg_sims = np.sort(X[np.array(c)].mean(axis=0))[::-1][:15]
+            i, avg_sim = [
+                (i, avg_sim) for i, avg_sim in zip(inds, avg_sims) if is_valid(words[i])
+            ][0]
+            if avg_sim > max_avg_sim:
+                word = words[i]
+                max_avg_sim = avg_sim
+                best_c = c
+
+    return word, [red_words[i] for i in best_c]
+
 
 # %%
-alt.Chart(df_emb).mark_circle().encode(
-    x="x", y="y", tooltip="label", color="color:N"
-).interactive()
+red_words
+
+# %%
+produce_clue(blue_words, red_words, neutral_words, assassin_words)
+
+# %%
+while len(red_words) > 0:
+    word, combination = produce_clue(red_words, blue_words, neutral_words, assassin_words)
+    print((word, combination))
+    for c in combination:
+        red_words.remove(c)
 
 # %%
