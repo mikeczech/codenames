3a2d54a715756c006ee7102249292cc82a1a52c6
Mike Czech <mail@mczech.de>
2020-12-21 12:34:56 +0000

    First working version

--- notebooks/word2vec.py
+++ notebooks/word2vec.py
@@ -14,7 +14,7 @@
 # ---
 
 # %% jupyter={"outputs_hidden": true}
-# !python -m spacy download en_core_web_md`````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````
+# !python -m spacy download en_core_web_lg
 
 # %%
 # %load_ext lab_black
@@ -25,14 +25,6 @@ import numpy as np
 from tqdm.notebook import tqdm
 
 from itertools import combinations
-
-# %%
-with open("../data/words_alpha.txt") as f:
-    words = f.readlines()
-words = [w.strip() for w in words]
-
-# %%
-len(words)
 
 # %%
 nlp = spacy.load("en_core_web_lg")
@@ -50,9 +42,6 @@ nlp.vocab[india].similarity(nlp.vocab[germany])
 
 # %%
 nlp.vocab[india].similarity(nlp.vocab[apple])
-
-# %%
-list(combinations(red_words, 2))
 
 
 # %%
@@ -89,10 +78,21 @@ blue_words = [
 neutral_words = ["jack", "point", "laser", "lemon", "unicorn", "pyramid", "figure"]
 assassin_words = ["hollywood"]
 
-all_words = red_words + blue_words + neutral_words + assassin_words
+vocab = list(nlp.vocab.strings)
+len(vocab)
 
 # %%
-X = np.array([[similarity(w_a, w) for w in words] for w_a in all_words])
+[(w, w in vocab) for w in red_words]
+
+# %%
+X = np.array([[similarity(w_a, w) for w in vocab] for w_a in red_words])
+X.shape
+
+# %%
+np.save("x.npy", X)
+
+# %%
+# !ls
 
 # %%
 X.shape
@@ -100,44 +100,34 @@ X.shape
 
 # %%
 def is_valid(suggested_word):
+    suggested_word = suggested_word.lower()
     for w in all_words:
+        w = w.lower()
         if (suggested_word in w) or (w in suggested_word):
             return False
     return True
 
 
-def produce_clue(red_words, blue_words, neutral_words, assassin_words):
-    N = len(red_words)
-    word = None
-    best_c = None
-    max_avg_sim = -np.inf
+def produce_clues(words, max_num_words=4):
+    N = len(words)
+    clues = {}
+    for l in tqdm(list(range(1, max_num_words + 1))):
+        for c in tqdm(list(combinations(range(N), l))):
+            inds = X[np.array(c)].mean(axis=0).argsort()[::-1][:35]
+            avg_sims = np.sort(X[np.array(c)].mean(axis=0))[::-1][:35]
 
-    for l in range(2, 3):
-        for c in combinations(range(N), l):
-            inds = X[np.array(c)].mean(axis=0).argsort()[::-1][:15]
-            avg_sims = np.sort(X[np.array(c)].mean(axis=0))[::-1][:15]
-            i, avg_sim = [
-                (i, avg_sim) for i, avg_sim in zip(inds, avg_sims) if is_valid(words[i])
-            ][0]
-            if avg_sim > max_avg_sim:
-                word = words[i]
-                max_avg_sim = avg_sim
-                best_c = c
+            for i, avg_sim in zip(inds, avg_sims):
+                if is_valid(vocab[i]):
+                    clues[tuple([words[j] for j in c])] = (
+                        vocab[i].lower(),
+                        avg_sim,
+                    )
+                    break
 
-    return word, [red_words[i] for i in best_c]
+    return clues
 
 
 # %%
-red_words
-
-# %%
-produce_clue(blue_words, red_words, neutral_words, assassin_words)
-
-# %%
-while len(red_words) > 0:
-    word, combination = produce_clue(red_words, blue_words, neutral_words, assassin_words)
-    print((word, combination))
-    for c in combination:
-        red_words.remove(c)
+produce_clues(red_words)
 
 # %%
