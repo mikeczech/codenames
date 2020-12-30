ecf55b959c5a05ac2767a419b276f9616cb69457
Mike Czech <mail@mczech.de>
2020-12-30 15:12:35 +0100

    Allow arbitrary choice of colors

--- notebooks/word2vec.py
+++ notebooks/word2vec.py
@@ -22,6 +22,7 @@
 # %%
 import spacy
 import numpy as np
+import pandas as pd
 from tqdm.notebook import tqdm
 
 from itertools import combinations
@@ -42,14 +43,6 @@ nlp.vocab[india].similarity(nlp.vocab[germany])
 
 # %%
 nlp.vocab[india].similarity(nlp.vocab[apple])
-
-
-# %%
-def similarity(word_a, word_b):
-    word_a = nlp.vocab.strings[word_a]
-    word_b = nlp.vocab.strings[word_b]
-    return nlp.vocab[word_a].similarity(nlp.vocab[word_b])
-
 
 # %%
 red_words = [
@@ -77,15 +70,25 @@ blue_words = [
 
 neutral_words = ["jack", "point", "laser", "lemon", "unicorn", "pyramid", "figure"]
 assassin_words = ["hollywood"]
-words = red_words + blue_words + neutral_words + assassin_words
-len(words)
+all_words = red_words + blue_words + neutral_words + assassin_words
+len(all_words)
+
+# %%
+df_all_words = pd.DataFrame(
+    {
+        "word": all_words,
+        "color": ["red"] * len(red_words)
+        + ["blue"] * len(blue_words)
+        + ["neutral"] * len(neutral_words)
+        + ["assassin"] * len(assassin_words),
+    }
+)
+
+df_all_words
 
 # %%
 vocab = list(nlp.vocab.strings)
 len(vocab)
-
-# %%
-[(w, w in vocab) for w in words]  # make sure that the vocab covers the words
 
 # %%
 similarities = np.array([[similarity(w_a, w) for w in vocab] for w_a in words])
@@ -96,6 +99,12 @@ np.save("similarities.npy", similarities)
 
 
 # %%
+def similarity(word_a, word_b):
+    word_a = nlp.vocab.strings[word_a]
+    word_b = nlp.vocab.strings[word_b]
+    return nlp.vocab[word_a].similarity(nlp.vocab[word_b])
+
+
 def is_valid(suggested_word):
     for w in words:
         w = w.lower()
@@ -104,29 +113,51 @@ def is_valid(suggested_word):
     return True
 
 
-def produce_clues(words, similarities, max_num_words=4, max_suggestions=35):
-    num_words = len(words)
-    clues = {}
-    for hint_len in tqdm(range(1, max_num_words + 1), total=max_num_words):
-        for selection in tqdm(list(combinations(range(num_words), hint_len))):
-            selection = np.array(selection)
-            mean_similarities = similarities[selection].mean(axis=0)
-            suggestion_indices = mean_similarities.argsort()[::-1][:max_suggestions]
-            suggestion_scores = mean_similarities[suggestion_indices]
+def produce_clues(
+    color, df_all_words, similarities, max_num_words=4, max_suggestions=35
+):
+    word_index = df_all_words[df_all_words.color == color].index
 
+    selection_col = []
+    suggestion_col = []
+    hint_len_col = []
+    score_col = []
+    for hint_len in tqdm(range(1, max_num_words + 1), total=max_num_words):
+        
+        for selection in tqdm(list(combinations(word_index, hint_len))):
+            selection = np.array(selection)
+            agg_similarities = similarities[selection].mean(axis=0)
+            
+            suggestion_indices = agg_similarities.argsort()[::-1][:max_suggestions]
+            suggestion_scores = agg_similarities[suggestion_indices]
             for index, score in zip(suggestion_indices, suggestion_scores):
                 suggested_word = vocab[index].lower()
                 if is_valid(suggested_word):
-                    clues[tuple([words[j] for j in selection])] = (
-                        suggested_word,
-                        score,
-                    )
+                    selection_words = tuple([words[j] for j in selection])
+                    selection_col.append(selection_words)
+                    suggestion_col.append(suggested_word)
+                    hint_len_col.append(hint_len)
+                    score_col.append(score)
                     break
 
-    return clues
+    return (
+        pd.DataFrame(
+            {
+                "selection": selection_col,
+                "suggestion": suggestion_col,
+                "hint_len": hint_len_col,
+                "score": score_col,
+            }
+        )
+        .sort_values(by="score", ascending=False)
+        .reset_index(drop=True)
+    )
 
 
 # %%
-produce_clues(words, similarities)
+df_clues = produce_clues("red", df_all_words, similarities)
+
+# %%
+df_clues.sort_values(by="score", ascending=False).head(20)
 
 # %%
