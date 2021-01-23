4da147327e6f677de8618b10c88d5069392a4cfc
Mike Czech <mail@mczech.de>
2021-01-23 18:45:07 +0100

    Add endpoint for game retrieval

--- codenames/codenames.py
+++ codenames/codenames.py
@@ -4,36 +4,52 @@ from flask import render_template
 from flask import redirect
 from flask import request
 from flask import url_for
-from flask import flash
+from flask import jsonify
 
 from codenames.db import get_db
 from codenames.errors import InvalidUsage
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
-@bp.route("/create", methods=("GET", "POST"))
+@bp.route("/create", methods=("POST", ))
 def create():
-    if request.method == "POST":
-        game_id = request.form["game-id"]
-        db = get_db()
-        error = None
+    game_id = request.form["game-id"]
+    db = get_db()
+    error = None
 
-        if not game_id:
-            error = "Invalid game id"
-        elif db.execute("SELECT name from games WHERE name = ?", (game_id,)).fetchone() is not None:
-            error = f"Game {game_id} already exists"
+    if not game_id:
+        error = "Invalid game id"
+    elif db.execute("SELECT name from games WHERE name = ?", (game_id,)).fetchone() is not None:
+        error = f"Game {game_id} already exists"
 
-        if error is None:
-            db.execute(
-                "INSERT INTO games (name) VALUES (?)",
-                (game_id,)
-            )
-            db.commit()
-        else:
-            raise InvalidUsage(f"An error occurred: {error}", status_code = 400)
+    if error is not None:
+        raise InvalidUsage(f"An error occurred: {error}", status_code = 400)
 
-    return render_template("create_game.html")
+    db.execute(
+        "INSERT INTO games (name) VALUES (?)",
+        (game_id,)
+    )
 
-@bp.route("/<game_id>")
-def game(game_id):
-    return render_template("game.html", game_id=game_id)
+    db.commit()
+    return jsonify(success=True)
+
+
+
+
+@bp.route("/game", methods=("GET",))
+def game():
+    game_id = request.args.get("game-id")
+    db = get_db()
+    error = None
+
+    if not game_id:
+        error = "Invalid game id"
+
+    game = db.execute("SELECT name from games WHERE name = ?", (game_id,)).fetchone()
+    if game is None:
+        error = f"Game {game_id} does not exist"
+
+    if error is not None:
+        raise InvalidUsage(f"An error occurred: {error}", status_code = 400)
+
+    return jsonify(name=game["name"])
