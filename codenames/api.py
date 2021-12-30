62c6f0f80c7cd5f50812ac38a6fff62eb2a2a6d8
Mike Czech <mike.czech@xing.com>
2021-12-30 12:34:46 +0100

    Add endpoint for joining a game

--- codenames/api.py
+++ codenames/api.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import Session
 
 from codenames import models, schemas
 from codenames.sql import SQLAlchemyGameManager, SQLAlchemyGameBackend
-from codenames.game import Game
+from codenames.game import (
+    Game,
+    Color,
+    Role,
+    RoleOccupiedException,
+    AlreadyJoinedException,
+)
 from codenames.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -49,6 +55,35 @@ def read_players(backend: SQLAlchemyGameBackend = Depends(get_game_backend)):
 
 @app.get("/games/{game_id}/conditions", response_model=List[schemas.Condition])
 def read_conditions(backend: SQLAlchemyGameBackend = Depends(get_game_backend)):
+    return backend.read_conditions()
+
+
+@app.post("/games/{game_id}/join")
+def join_game(
+    color_id: int = Form(...),
+    role_id: int = Form(...),
+    session_id: Optional[str] = Cookie(None),
+    backend: SQLAlchemyGameBackend = Depends(get_game_backend),
+):
+    if session_id is None:
+        raise HTTPException(status_code=401, detail="Could not determine session id")
+    current_game_state = Game(session_id, backend).load_state()
+    try:
+        current_game_state.join(Color(color_id), Role(role_id))
+    except RoleOccupiedException:
+        raise HTTPException(
+            status_code=403,
+            detail="The given color/role is already occupied by another player",
+        )
+    except AlreadyJoinedException:
+        raise HTTPException(
+            status_code=403, detail="It seems you already joined the game"
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=400,
+            detail="Cannot join the game (maybe the game is already running)",
+        )
     return backend.read_conditions()
 
 
