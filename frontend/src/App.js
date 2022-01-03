d5e1a0cd504cfa3de61b12a4505aaeb3310703e7
Mike Czech <mike.czech@xing.com>
2022-01-03 21:38:50 +0100

    Allow joining the game via the React UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import Cookies from 'universal-cookie';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,6 +8,11 @@ import {
   Route,
   useParams
 } from "react-router-dom";
+
+const RED_COLOR_ID = 1;
+const BLUE_COLOR_ID = 2;
+const PLAYER_ROLE_ID = 1;
+const SPYMASTER_ROLE_ID = 2;
 
 export default function App() {
   return (
@@ -107,15 +114,44 @@ function colorIdToClass(colorId) {
 function Game() {
   const { gameId } = useParams();
   const [words, setWords] = useState(null);
+  const [playerName, setPlayerName] = useState(null);
 
   useEffect(() => {
       fetch(`/games/${gameId}/words`).then(res => res.json()).then(data => {
         setWords(data)
       });
+      const cookies = new Cookies();
+      if(typeof cookies.get("session_id") === 'undefined') {
+        cookies.set("session_id", uuidv4(), { path: '/'});
+      };
   }, []);
 
   function renderSquare(w) {
     return <li><Square word={w["word"]} colorClass={colorIdToClass(w["color"])}/></li>
+  }
+
+  function handleJoin(colorId, roleId) {
+    (async () => {
+      fetch(`/games/${gameId}/join`, {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({color_id: colorId, role_id: roleId, name: playerName})
+      })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({created: true, gameId: json['game_id']})
+      })
+      .catch(e => {
+        console.log(e)
+      });
+    })();
+  }
+
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
   }
 
   if (!words) {
@@ -128,19 +164,19 @@ function Game() {
       <form>
         <label>
           Username:
-          <input type="text" value="username" />
+          <input type="text" onChange={e => handlePlayerNameChange(e)} />
         </label>
         <div>
-          <button>Join as spymaster (red)</button>
+          <button onClick={() => handleJoin(RED_COLOR_ID, SPYMASTER_ROLE_ID)}>Join as spymaster (red)</button>
         </div>
         <div>
-          <button>Join as agent (red)</button>
+          <button onClick={() => handleJoin(RED_COLOR_ID, PLAYER_ROLE_ID)}>Join as agent (red)</button>
         </div>
         <div>
-          <button>Join as spymaster (blue)</button>
+          <button onClick={() => handleJoin(BLUE_COLOR_ID, SPYMASTER_ROLE_ID)}>Join as spymaster (blue)</button>
         </div>
         <div>
-          <button>Join as agend (blue)</button>
+          <button onClick={() => handleJoin(BLUE_COLOR_ID, PLAYER_ROLE_ID)}>Join as agend (blue)</button>
         </div>
       </form>
     </div>
